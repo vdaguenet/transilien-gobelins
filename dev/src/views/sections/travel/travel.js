bf735a0f3e0e4e863fb3231787a95d7e6152b9c9
Valentin Daguenet <valentin.daguenet@gmail.com>
2014-10-29 23:49:09 +0100

    Just a little train

--- dev/src/views/sections/travel/travel.js
+++ dev/src/views/sections/travel/travel.js
@@ -6,6 +6,7 @@ var Vue = require('vue'),
     section = require('base/section'),
     forEach = require('forEach'),
     resizeUtil = require('common/utils/resize-util'),
+    scrollUtil = require('common/utils/scroll-util'),
     bindAll = require('bindall-standalone');
 
 module.exports = extend(true, {}, section, {
@@ -26,7 +27,9 @@ module.exports = extend(true, {}, section, {
             {
                 bg: 'assets/images/travel-section-3.jpg'
             }
-        ]
+        ],
+        scrollInit: false,
+        userHasScrolled: false
     },
     components: {
         'travel-section': require('./components/travel-section/travel-section')
@@ -36,18 +39,40 @@ module.exports = extend(true, {}, section, {
             this.tlTransition.to(window, 0.6, {scrollTo: {y: this.$el.scrollHeight,x: 0}});
         },
         beforeTransitionIn: function() {},
+        onTransitionInComplete: function() {
+            this.scrollInit = true;
+        },
         resize: function() {
-            TweenMax.set(window, {scrollTo: {y: this.$el.scrollHeight,x: 0}});
+            if(true === this.scrollInit && false === this.userHasScrolled) {
+                TweenMax.set(window, {scrollTo: {y: this.$el.scrollHeight,x: 0}});
+            }
+
+            var train = this.$findOne('.train');
+            var trainHalfW = Math.floor(train.offsetWidth / 2);
+            TweenMax.set(train, {x: resizeUtil.halfWidth - trainHalfW});
+        },
+        scroll: function () {
+            if(false === this.scrollInit) return;
+
+            this.userHasScrolled = true;
         },
         init: function() {
+            var train = this.$findOne('.train');
+            var trainHalfW = Math.floor(train.offsetWidth / 2);
+            TweenMax.set(train, {x: resizeUtil.halfWidth - trainHalfW});
+
             resizeUtil.addListener(this.resize);
+            scrollUtil.addListener(this.scroll);
         }
     },
 
     ready: function() {
-        bindAll(this, 'resize', 'init');
+        bindAll(this, 'resize', 'scroll', 'init');
         Vue.nextTick(this.init);
     },
 
-    beforeDestroy: function() {}
+    beforeDestroy: function() {
+        resizeUtil.removeListener(this.resize);
+        scrollUtil.removeListener(this.scroll);
+    }
 });
