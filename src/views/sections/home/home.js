0ff2befe44bc27e846b512b06a46ed3e5d7fe090
Valentin Daguenet <valentin.daguenet@gmail.com>
2014-12-12 23:10:50 +0100

    Button hover animation

--- src/views/sections/home/home.js
+++ src/views/sections/home/home.js
@@ -24,7 +24,7 @@ module.exports = extend(true, {}, section, {
             this.tlTransition.fromTo(this.$findOne('.cloud.left'), 0.8, {xPercent: '-100%'}, {xPercent: 0, ease: Expo.easeOut}, 0.7);
             this.tlTransition.fromTo(this.$findOne('.cloud.right'), 0.8, {xPercent: '100%'}, {xPercent: 0, ease: Expo.easeOut}, 0.7);
             this.tlTransition.staggerFromTo(
-                [this.$findOne('.content img'), this.$findOne('.content h1'), this.$findOne('.button')],
+                [this.$findOne('.content img'), this.$findOne('.content h1'), [this.$findOne('.button'), this.$findOne('.button-background')]],
                 1.0,
                 {y: 100, alpha: 0},
                 {y: 0, alpha: 1, ease: Expo.easeOut},
@@ -32,7 +32,29 @@ module.exports = extend(true, {}, section, {
                 1.2);
         },
         beforeTransitionIn: function() {
+            this.resize();
+        },
+        onMouseEnter: function () {
+            if(this.tlMouseOut) this.tlMouseOut.kill();
 
+            var points = this.$findOne('.button-background polygon').points;
+            this.tlMouseEnter = new TimelineMax();
+            this.tlMouseEnter.set(points[0], {x: 0}, 0);
+            this.tlMouseEnter.set(points[3], {x: 0}, 0);
+            this.tlMouseEnter.fromTo(points[2], 0.4, {x: 0}, {x: 170, ease: Expo.easeOut}, 0);
+            this.tlMouseEnter.fromTo(this.$findOne('.button'), 0.4, {color: '#0087CD'}, {color: '#e7e7e7', ease: Expo.easeOut}, 0);
+            this.tlMouseEnter.fromTo(points[1], 0.4, {x: 0}, {x: 170, ease: Expo.easeOut}, 0.06);
+        },
+        onMouseOut: function () {
+            if(this.tlMouseEnter) this.tlMouseEnter.kill();
+
+            var points = this.$findOne('.button-background polygon').points;
+            this.tlMouseOut = new TimelineMax();
+            this.tlMouseOut.set(points[1], {x: 170}, 0);
+            this.tlMouseOut.set(points[2], {x: 170}, 0);
+            this.tlMouseOut.fromTo(points[3], 0.4, {x: 0}, {x: 170, ease: Expo.easeOut}, 0);
+            this.tlMouseOut.fromTo(this.$findOne('.button'), 0.4, {color: '#e7e7e7'}, {color: '#0087CD', ease: Expo.easeOut}, 0);
+            this.tlMouseOut.fromTo(points[0], 0.4, {x: 0}, {x: 170, ease: Expo.easeOut}, 0.06);
         },
         resize: function() {
             TweenMax.set(this.$el, {width: resizeUtil.width, height: resizeUtil.height});
@@ -46,20 +68,25 @@ module.exports = extend(true, {}, section, {
             }
         },
         init: function() {
-            this.resize();
-            // TEST
-            TweenMax.set(this.$findOne('.content svg'), {scaleX: 2});
             resizeUtil.addListener(this.resize);
             scrollUtil.addListener(this.scroll);
         }
     },
 
     ready: function() {
+        this.$once('section:transitionInComplete', function() {
+            this.$findOne('.button').addEventListener('mouseenter', this.onMouseEnter.bind(this));
+            this.$findOne('.button').addEventListener('mouseleave', this.onMouseOut.bind(this));
+        });
+
         bindAll(this, 'init', 'resize', 'scroll');
         Vue.nextTick(this.init);
     },
 
     beforeDestroy: function() {
         resizeUtil.removeListener(this.resize);
+        scrollUtil.removeListener(this.scroll);
+        this.$findOne('.button').removeEventListener('mouseenter', this.onMouseEnter.bind(this));
+        this.$findOne('.button').removeEventListener('mouseleave', this.onMouseOut.bind(this));
     }
 });
